2c5c760f1d566deb123a08149f554a5f26d0db68
AniShin <minecraft2727k@gmail.com>
2024-08-31 16:12:52 +0530

    Update start_&_cb.py

--- plugins/start_&_cb.py
+++ plugins/start_&_cb.py
@@ -43,8 +43,25 @@ async def start(client, message):
 
 @Client.on_message(filters.command('start') & filters.private)
 async def not_premium(client, message):
-button = 
-
+    buttons = [
+        [
+            InlineKeyboardButtons(text="🍂Buy Premium🍂", url="tg://user?id=1768198143")
+        ]
+    ]
+    await message.reply_photo(
+        Config.Pre_img,
+        caption=Txt.Pre_msg.format(
+            first=message.from_user.first_name,
+            last=message.from_user.last_name,
+            username=None if not message.from_user.username else '@' + message.from_user.username,
+            mention=message.from_user.mention,
+            id=message.from_user.id
+        ),
+        reply_markup=InlineKeyboardMarkup(buttons),
+        quote=True,
+        disable_web_page_preview=True
+        
+    
 @Client.on_callback_query()
 async def cb_handler(client, query: CallbackQuery):
     data = query.data 
