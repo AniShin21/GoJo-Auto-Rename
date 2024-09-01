0f7e5b2675440d9a1868266a1b348d8d9d5fea2b
AniShin <minecraft2727k@gmail.com>
2024-09-01 21:01:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,8 +17,8 @@ class Config(object):
     BOT_UPTIME  = time.time()
     START_PIC   = os.environ.get("START_PIC", "https://graph.org/file/4b306f4b15c23a8f22e58.jpg")
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6450266465').split()]
-    FORCE_SUB   = os.environ.get("FORCE_SUB", "") 
-    LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "")
+    FORCE_SUB   = int(os.environ.get("FORCE_SUB", ""))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
 
     # Image For User Is not In premium
     Pre_img = os.environ.get("Preimg", "https://graph.org/file/4b306f4b15c23a8f22e58.jpg")
