fa60b7e1442e1d7559d96f872269bafba5944b8c
AniShin <minecraft2727k@gmail.com>
2024-09-01 20:50:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ class Config(object):
     START_PIC   = os.environ.get("START_PIC", "https://graph.org/file/4b306f4b15c23a8f22e58.jpg")
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6450266465').split()]
     FORCE_SUB   = os.environ.get("FORCE_SUB", "") 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+    LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "")
 
     # Image For User Is not In premium
     Pre_img = os.environ.get("Preimg", "https://graph.org/file/4b306f4b15c23a8f22e58.jpg")
